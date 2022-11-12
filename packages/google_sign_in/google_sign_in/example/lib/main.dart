ab385774ff52755c8e5fa5e59ccdf3eb02d8e007
David Iglesias Teixeira <ditman@gmail.com>
2022-11-11 19:57:59 -0800

    Wire example app

--- packages/google_sign_in/google_sign_in/example/lib/main.dart
+++ packages/google_sign_in/google_sign_in/example/lib/main.dart
@@ -15,7 +15,7 @@ GoogleSignIn _googleSignIn = GoogleSignIn(
   // Optional clientId
   // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
   scopes: <String>[
-    'email',
+    // 'email',
     'https://www.googleapis.com/auth/contacts.readonly',
   ],
 );
